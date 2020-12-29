027e4ba7ae4a91440f223316c3747201de136990
João Silvestre <joaopaulosilvestre@hotmail.com>
2020-12-29 19:21:31 +0000

    Continuing to work on the responsiveness, still some issues with the graphs canvas making everything weird
    ToolName is now properly showing and updating

--- Silvestre.Psychology.Tools.WISC3.WebComponent/ViewModel/PsychologyToolsViewModel.cs
+++ Silvestre.Psychology.Tools.WISC3.WebComponent/ViewModel/PsychologyToolsViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Silvestre.Psychology.Tools.WISC3.WebComponent.ViewModel
+﻿using System;
+
+namespace Silvestre.Psychology.Tools.WISC3.WebComponent.ViewModel
 {
     public class PsychologyToolsViewModel
     {
-        public string ToolName { get; set; }
+        private string? _toolName;
+        public string? ToolName
+        {
+            get => _toolName;
+            set
+            {
+                _toolName = value;
+                StateChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler? StateChanged;
     }
 }
