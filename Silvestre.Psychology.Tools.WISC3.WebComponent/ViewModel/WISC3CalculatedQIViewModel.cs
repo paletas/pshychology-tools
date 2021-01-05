51652e227ed3a7b620f35e46a4ea6917981c258e
João Silvestre <joaopaulosilvestre@hotmail.com>
2021-01-05 21:55:49 +0000

    Added a comparison to a standard average for the QI value

--- Silvestre.Psychology.Tools.WISC3.WebComponent/ViewModel/WISC3CalculatedQIViewModel.cs
+++ Silvestre.Psychology.Tools.WISC3.WebComponent/ViewModel/WISC3CalculatedQIViewModel.cs
@@ -1,10 +1,35 @@
 ﻿using Silvestre.Psychology.Tools.WISC3.Calculator;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Silvestre.Psychology.Tools.WISC3.WebComponent.ViewModel
 {
+    public enum AverageComparisonResultEnum
+    {
+        ExtremelyBelow,
+        FarBelow,
+        Below,
+        OnAverage,
+        Above,
+        FarAbove,
+        ExtremelyAbove
+    }
+
     public class WISC3CalculatedQIViewModel
     {
+
+        private static IDictionary<Range, AverageComparisonResultEnum> _ComparisonTable = new Dictionary<Range, AverageComparisonResultEnum>
+        {
+            { 0..69, AverageComparisonResultEnum.ExtremelyBelow },
+            { 70..79, AverageComparisonResultEnum.FarBelow },
+            { 80..89, AverageComparisonResultEnum.Below },
+            { 90..109, AverageComparisonResultEnum.OnAverage },
+            { 110..119, AverageComparisonResultEnum.Above },
+            { 120..129, AverageComparisonResultEnum.FarAbove },
+            { 130..int.MaxValue, AverageComparisonResultEnum.ExtremelyAbove }
+        };
+
         private readonly Func<short, QI?> _calculateQI;
         private short? _standardResult;
 
@@ -34,6 +59,18 @@ namespace Silvestre.Psychology.Tools.WISC3.WebComponent.ViewModel
         public (short LowerBound, short UpperBound)? ConfidenceInterval90 { get; private set; }
 
         public (short LowerBound, short UpperBound)? ConfidenceInterval95 { get; private set; }
+
+        public AverageComparisonResultEnum? AverageComparisonResult 
+        { 
+            get 
+            {
+                if (IndexQI.HasValue)
+                {
+                    return _ComparisonTable.Single(kv => kv.Key.Start.Value <= IndexQI.Value && kv.Key.End.Value >= IndexQI.Value).Value;
+                }
+                else return null;
+            }
+        }
 
         private void CalculateQI()
         {
